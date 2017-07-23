9a0169648e711c602d9f9ec8cbf2196e7d76c020
Ryan Jackson <ryanjackson0y-@live.com>
2017-07-23 16:26:45 -0500

    Implement main program logic.

--- ExercisesForProgrammers/Program.cs
+++ ExercisesForProgrammers/Program.cs
@@ -6,7 +6,21 @@ namespace ExercisesForProgrammers
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the bill amount? ");
+			string billInput = Console.ReadLine();
+            var newBill = new Bill() { Subtotal = Convert.ToDouble(billInput.Trim()) };
+
+            Console.WriteLine("What is the tip percentage? ");
+            string tipInput = Console.ReadLine();
+            var percentageOfSubtotal = Convert.ToInt32(tipInput.Trim());
+            var newTip = new Tip(newBill.Subtotal, percentageOfSubtotal); //Assignment of subtotal is a bit odd here.
+            newBill.Tip = newTip;
+
+            Console.WriteLine($"The tip is {newTip.Value}. ");
+
+            Console.WriteLine($"The total is {newBill.Total}. ");
+
+            Console.ReadLine(); // Wait for user to end program.
         }
     }
 }
