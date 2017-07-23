a86a18e7ca3dd618e848f9132ab85bf547535d3c
Ryan Jackson <ryanjackson0y-@live.com>
2017-07-23 15:43:38 -0500

    * .gitignore: Initial - Flesh out model classes for Tip Calculator   based on OOP instinct.
    * ExercisesForProgrammers.sln:
    * Tip.cs:
    * Bill.cs:
    * Program.cs:
    * AssemblyInfo.cs:
    * ExercisesForProgrammers.csproj:

---
+++ ExercisesForProgrammers/Bill.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ExercisesForProgrammers
+{
+    public class Bill
+    {
+        private double _subtotal;
+        public double Subtotal { 
+            get {
+				return _subtotal;
+			}
+			set {
+	            this.Tip.Calculate(value, this.Tip.PercentageOfSubtotal);
+				_subtotal = value;
+			}
+        }
+
+        private Tip _tip;
+        public Tip Tip
+        {
+			get
+			{
+                return _tip;
+			}
+			set
+			{
+                this.Total = value.Value;
+				_subtotal = value;
+			}
+        }
+
+        public double Total { get; private set; }
+
+
+        public Bill()
+        {
+            this.Tip = new Tip();
+        }
+    }
+}
---
+++ ExercisesForProgrammers/Tip.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExercisesForProgrammers
+{
+    public class Tip
+    {
+        public double Value { get; private set; }
+
+        // Feels a bit weird, as subtotal of a bill is not really part of a tip.
+        // Including it here, because it is needed to calculate a tip's value,
+        //   and can be used for program consistency checking.
+        private double _subtotal;
+        public double Subtotal {
+			get
+			{
+				return _subtotal;
+			}
+			set
+			{
+                this.Calculate(value, _percentageOfSubtotal);
+				_subtotal = value;
+			}        
+        }
+
+        private int _percentageOfSubtotal;
+        public int PercentageOfSubtotal { 
+            get {
+                return _percentageOfSubtotal;
+            }
+            set {
+                this.Calculate(_subtotal, value);
+                _percentageOfSubtotal = value;
+            }
+        }
+
+        public Tip()
+        {
+        }
+
+        public Tip(double subtotal, int percentageOfSubtotal) {
+            this.Value = this.Calculate(subtotal, percentageOfSubtotal);
+        }
+
+        public double Calculate(double subtotal, int percentageOfSubtotal) {
+            var result = subtotal * (percentageOfSubtotal / 100);
+            this.Value = result;
+            return result;
+        }
+    }
+}
